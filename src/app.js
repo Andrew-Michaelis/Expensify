413a987e8f50ebd8266f15f8a484081dd6d211ea
Andrew-Michaelis <amichaelis70@s.tooeletech.edu>
2021-08-25 16:59:08 -0600

    d5

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
 import AppRouter from "./routers/AppRouter";
 import configureStore from "./store/configureStore";
 import "normalize.css/normalize.css";
@@ -11,12 +12,19 @@ import getVisibleExpenses from "./selectors/expenses";
 
 const store = configureStore();
 
-store.dispatch(addExpense({ description: "Water Bill" }));
-store.dispatch(addExpense({ description: "Gass Bill" }));
-store.dispatch(setTextFilter("bill"));
+store.dispatch(addExpense({ description: "Water Bill", amount: 4500 }));
+store.dispatch(addExpense({ description: "Rent", amount: 109500 }))
+store.dispatch(addExpense({ description: "Gas Bill" }));
+store.dispatch(addExpense({ description: "Apple Bushel", amount: 5000 }))
 
 const state = store.getState();
 const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
 console.log(visibleExpenses);
 
-ReactDOM.render(<AppRouter />, document.getElementById("app"));
+const jsx = (
+    <Provider store={store}>
+        <AppRouter />
+    </Provider>
+);
+
+ReactDOM.render(jsx, document.getElementById("app"));
--- src/selectors/expenses.js
+++ src/selectors/expenses.js
@@ -9,7 +9,7 @@ export default (expenses, { text, sortBy, startDate, endDate })=> {
         if(sortBy === "date") {
             return a.createdAt < b.createdAt ? 1 : -1;
         }
-        if(sortBy === "amount") {
+        else if(sortBy === "amount") {
             return a.amount < b.amount ? 1 : -1;
         }
     })
