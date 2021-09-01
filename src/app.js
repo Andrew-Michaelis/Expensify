b649dbcdbb2f8b8f2b34aade202cdeb164248679
Andrew-Michaelis <amichaelis70@s.tooeletech.edu>
2021-09-01 15:43:20 -0600

    d8.5

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import AppRouter from "./routers/AppRouter";
 import configureStore from "./store/configureStore";
+//-------------------------CSS Imports
 import "normalize.css/normalize.css";
 import "./styles/styles.scss";
 import "react-dates/lib/css/_datepicker.css";
@@ -12,15 +13,6 @@ import { setTextFilter } from "./actions/filters";
 import getVisibleExpenses from "./selectors/expenses";
 
 const store = configureStore();
-
-store.dispatch(addExpense({ description: "Water Bill", amount: 4500 }));
-store.dispatch(addExpense({ description: "Rent", amount: 109500 }))
-store.dispatch(addExpense({ description: "Gas Bill" }));
-store.dispatch(addExpense({ description: "Apple Bushel", amount: 5000 }))
-
-const state = store.getState();
-const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-console.log(visibleExpenses);
 
 const jsx = (
     <Provider store={store}>
