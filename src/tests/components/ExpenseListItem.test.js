9bdd6ae4ff6460de69494e59b46e075aa1e8268e
Andrew-Michaelis <amichaelis70@s.tooeletech.edu>
2021-09-07 15:52:20 -0600

    setup formatting for amount and createdAt

--- src/tests/components/ExpenseListItem.test.js
+++ src/tests/components/ExpenseListItem.test.js
@@ -6,4 +6,4 @@ import expenses from "../fixtures/expenses";
 test("should render ExpenseListItem correctly", ()=> {
     const wrapper = shallow(<ExpenseListItem {...expenses[2]}/>);
     expect(wrapper).toMatchSnapshot();
-})
+});
